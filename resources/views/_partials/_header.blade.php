72eddf13e7b962d6ead061d44c331d93499d4600
eastfiles <feyswallmkota92@gmail.com>
2022-10-07 21:10:25 +0300

    last

--- resources/views/_partials/_header.blade.php
+++ resources/views/_partials/_header.blade.php
@@ -1,7 +1,7 @@
 <div class="container d-flex justify-content-between align-items-center">
 
     <div class="logo">
-      <h1><a href="index.html">Eterna</a></h1>
+      <h1><a href="{{ route("/") }}">BASMALL INTERNATIONAL CO.LTD</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
     </div>
