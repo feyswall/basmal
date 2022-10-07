154a62881bb08c3df9598fb5c2754dad19d04122
feyswal <feyswal@kali.kali>
2022-10-07 13:49:17 +0200

    everthing is there

---
+++ resources/views/_partials/_header.blade.php
@@ -0,0 +1,13 @@
+<div class="container d-flex justify-content-between align-items-center">
+
+    <div class="logo">
+      <h1><a href="index.html">Eterna</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+    </div>
+
+    <nav id="navbar" class="navbar">
+      @include("_partials._navigation")
+    </nav><!-- .navbar -->
+
+  </div>
